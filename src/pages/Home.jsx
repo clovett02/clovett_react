b4eee9ec39a3e0a036b807ca42d3449e16af5285
Chris <chris.lovett02@outlook.com>
2025-03-18 23:58:58 -0400

    Added git repo and linkedin links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import headshot from '../images/headshot.png';
+import '../css/home.css'
 
 export default function Home(){
     return(
@@ -11,15 +12,33 @@ export default function Home(){
                 </picture>
             </div>
             <h1>About Me</h1>
-                <p>I'm Chris Lovett. I'm a data scientist and software developer.
+                <p>&emsp;I'm Chris Lovett. I'm a data scientist and software developer.
                     When I'm not writing code or coming up with a way to effectively 
                     present data, I like to spend time with my wife and kids, work out,
                     and learn new things.
                 </p>
-                <p>If you want to learn more about my professional experience, please 
+                <p>&emsp;If you want to learn more about my professional experience, please 
                     click the link to my Resume page and also take a look at some of 
                     the projects that I've done, and others that I'm currently working on.
                 </p>
+                <p>
+            <div class="links">
+                <h4>Github Repositories</h4>
+                <a href="https://github.com/clovett02?tab=repositories">
+                    https://github.com/clovett02?tab=repositories</a><p></p>
+
+                <h4>LinkedIn Profile</h4>
+                <div class="badge-base LI-profile-badge" data-locale="en_US" 
+                    data-size="medium" data-theme="light" data-type="HORIZONTAL" 
+                    data-vanity="chris-lovett-7b1207b6" data-version="v1">
+                    <a class="badge-base__link LI-simple-link" 
+                    href="https://www.linkedin.com/in/chris-lovett-7b1207b6?trk=profile-badge">
+                    https://www.linkedin.com/in/chris-lovett-7b1207b6/</a>
+                </div>
+            </div>
+
+
+        </p>
         </div>
     );
 
