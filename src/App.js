893a8e7f7d86974d35b1eac48cc9c3a40de04a8b
chris <chris.lovett02@outlook.com>
2025-03-18 22:26:31 -0400

    Moved sidepanel to index.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import './App.css';
 import './css/main.css';
 import { BrowserRouter, Routes, Route } from 'react-router';
-import Sidepanel from './components/Sidepanel';
+// import Sidepanel from './components/Sidepanel';
 import Home from './pages/Home';
 import Resume from './pages/Resume';
 import Projects from './pages/Projects';
-import { Component } from 'react';
 
 export default function App(){
     return (
       <div className="App">
-        <body>
             <BrowserRouter>
               <Routes>
-                <Route element={<Sidepanel />}>
                   <Route path='/' element={<Home />} />
-                  <Route path='resume' element={<Resume />} />
-                  <Route path='projects' element={<Projects />} />
-                </Route>
+                  <Route path='/resume' element={<Resume />} />
+                  <Route path='/projects' element={<Projects />} />
               </Routes>
             </BrowserRouter>
-        </body>
         
       </div>
     )
