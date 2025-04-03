d6369fa1e2fff688362629c8916f799079412eb3
Chris <chris.lovett02@outlook.com>
2025-04-03 03:28:09 -0400

    Began work on the Projects page/component

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Projects(props) {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <p>Here are some of my projects:</p>
+      <h2>Job App Tracker</h2>
+      <h2>Gitlab (local docker instance)</h2>
+    </div>
+  );
+}
