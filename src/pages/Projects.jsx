f4b211f244ea8051ed1e73a683e76ed5c05e65a2
Chris <chris.lovett02@outlook.com>
2025-04-09 03:18:31 -0400

    Added more detail to existing descriptions

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -9,15 +9,19 @@ export default function Projects(props) {
     <ul className='projects'>
         <li className="project">
             <span className="pics">
-                <img src={jh1} alt="Job Hunt 1"/>
-                <img src={jh2} alt="Job Hunt 2"/>
+                <a href={jh1}>
+                    <img src={jh1} alt="Job Hunt 1" />
+                </a>
+                <a href={jh2}>
+                    <img src={jh2} alt="Job Hunt 2" />
+                </a>
             </span>
             <span className="description">
                 <h2>Job App Tracker</h2>
-                    <p>A web application to track job applications. Built with React, and 
-                        containerized as a docker image. It creates, reads, and updates job application 
-                        information from an API, written in C#, that interacts with a local MySQL instance on
-                        my home server.</p>
+                    <p>This is a simple web application that I use to track job applications. I build it with
+                        React, and it runs in a docker container on my homeserver. It creates, reads, and 
+                        updates job application information from an API, written in C#, that interacts with 
+                        a local MySQL docker instance, also on my home server.</p>
                 </span>
             <span className="skills">
                 <h3>Skills/Topics Demonstrated</h3>
@@ -33,12 +37,22 @@ export default function Projects(props) {
         </li>
         <li className='project'>
             <span className="pics">
-                <img src={gl1} alt="Gitlab 1"/>
-                <img src={gl2} alt="Gitlab 2"/>
+                <a href={gl1}>
+                    <img src={gl1} alt="Gitlab 1" />
+                </a>
+                <a href={gl2}>
+                    <img src={gl2} alt="Gitlab 2" />
+                </a>
             </span>
             <span className="description">
                 <h2>Gitlab (local docker instance)</h2>
-                <p>I have a local instance of Gitlab running on my linux server in a docker container. </p>
+                <p>I have a local instance of Gitlab running on my linux server in a docker container. Making use
+                    of Gitlab CI/CD has greatly sped up the process of building, testing, and deploying web apps,
+                    docker images, python packages, and other applications that I work on. After the initial setup 
+                    and configuration of the associted .gitlab-ci.yml file, the whole build, test, and deploy 
+                    process for a project is quick and painlessly kicked off with a push to the main branch of that 
+                    projects repository.
+                </p>
             </span>
             <span className="skills">
                 <h3>Skills/Topics Demonstrated</h3>
