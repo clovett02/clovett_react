482a2fb92b3a8894adfa3f2a2cb27bed1edeb848
Chris <chris.lovett02@outlook.com>
2025-06-05 20:34:46 -0400

    Added fadein fadeout animations for project pics

--- src/components/Slideshow.tsx
+++ src/components/Slideshow.tsx
@@ -6,33 +6,43 @@ export default function Slideshow(props:{pics:String[], alts:String[]}) {
     const pics = props.pics;
     const alts = props.alts;
 
-    const [picindex, setpicindex] = useState(0);
     let index = 0;
+    let previndex = pics.length - 1;
+    const [picindex, setpicindex] = useState(index);
+    const [prevpicindex, setprevpicindex] = useState(index);
+    
 
     useEffect( () => {
 
-        const interval = setInterval(() => {            
-            setpicindex(nextImage());
-        }, 6000);
+        const interval = setInterval(() => {
+            setprevpicindex(picindex);
+            setpicindex(nextImage(picindex));
+            // console.log(picindex);
+        }, 8000);
         return () => {clearInterval(interval)};
-    }, [])
+    }, [picindex, previndex, nextImage])
 
-    function nextImage(){
-        if(index === (pics.length - 1)){ 
-            index = 0;
-            return index;
+    function nextImage(ind: number){
+        if(ind === (pics.length - 1)){ 
+            ind = 0;
+            return ind;
         }
         else {
-            index = index + 1;
-            return index;
+            ind = ind + 1;
+            return ind;
         }
     }
 
     return(
         <span className="pics">
-            <a href={`${pics[picindex]}`}>
-                <img src={`${pics[picindex]}`} alt={`${alts[picindex]}`} />
-            </a>
+            <span>
+                <a className="currentpic" href={`${pics[picindex]}`}>
+                    <img src={`${pics[picindex]}`} alt={`${alts[picindex]}`} />
+                </a>
+                <a className="prevpic" href={`${pics[prevpicindex]}`}>
+                    <img src={`${pics[prevpicindex]}`} alt={`${alts[prevpicindex]}`}/>
+                </a>
+            </span>
         </span>
     );
 }
